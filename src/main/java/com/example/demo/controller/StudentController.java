740cf50377722199336e92bd79ff94e56fd4c07b
dhamithdev <dhamith@bluebaylabs.co>
2023-05-02 01:06:08 +0530

    changes in StudentController add list of student

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +27,10 @@ public class StudentController {
 	}
 	
 	// add multiple student
+	@PostMapping("/saveallstudent")
+	public List<Student> addMultipleStudent(@RequestBody List<Student> st){
+		return StRepo.saveAll(st);
+	}
 	
 	//find all student
 	
